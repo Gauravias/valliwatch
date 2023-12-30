bdfa59b3b97f0f5b9b35e9b75b34866278036d6b
Gaurav Tripathi <>
2023-12-30 08:06:45 +0530

    e

---
+++ script.js
@@ -0,0 +1,36 @@
+window.onload = function() {
+    const timeToDegrees = (hand, value) =>
+      hand === "hours" ? `${value * 30 + 180}deg` : `${value * 6 + 180}deg`;
+  
+    function setClock() {
+      let hours = new Date().getHours();
+      let minutes = new Date().getMinutes();
+      let seconds = new Date().getSeconds();
+  
+      let time = {
+        hours,
+        minutes,
+        seconds
+      };
+  
+      Object.entries(time).forEach(handEl => {
+        let hand = document.querySelector(`.${handEl[0]}`);
+  
+        if (handEl[1] == 0) {
+          hand.style.transition = "none";
+        } else {
+          hand.style.transition = "all 0.1s cubic-bezier(0.4, 2.08, 0.55, 0.44)";
+        }
+  
+        hand.style.transform = `translatex(-50%) rotate(${timeToDegrees(
+          handEl[0],
+          handEl[1]
+        )})`;
+      });
+      return seconds;
+    }
+  
+    setClock();
+    setInterval(setClock, 1000);
+  };
+  
